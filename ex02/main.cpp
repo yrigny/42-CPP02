ee9c4091c85b5f9bd1306a22470a8bea5c70f960
Yifan Rigny <yrigny@made-f0Ar6s8.clusters.42paris.fr>
2024-07-09 21:48:51 +0200

    minor norm change

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yrigny <yrigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:59:35 by yrigny            #+#    #+#             */
-/*   Updated: 2024/07/09 18:57:52 by yrigny           ###   ########.fr       */
+/*   Updated: 2024/07/09 19:03:16 by yrigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@ int main(void) {
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-
 	std::cout << b << std::endl;
-
 	std::cout << Fixed::max(a, b) << std::endl;
 
 	a = Fixed(42);
 	Fixed	c(4.2f);
+
 	std::cout << std::endl << "Extra tests with " << "a = " << a << ", c = " << c << std::endl;
 	std::cout << "test feature  | " << "test case | " << "test result" << std::endl;
 	std::cout << "operator '>'  | " << "a > c     | " << (a > c) << std::endl;
